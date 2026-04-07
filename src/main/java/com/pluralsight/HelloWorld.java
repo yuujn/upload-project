84ae96ae322f3186698f26fb36687f8302077e3d
yuujn <pale.cup5223@fastmail.com>
2026-04-07 15:42:23 -0500

    Print something else in the main method.

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world.");
+        System.out.println("Goodbye, world.");
     }
 }
